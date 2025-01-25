e0c903fcba3ca401a19c9dd33be3b86cb7b1edf5
ubaidullahshahid <ubaidullahshahid098@gmail.com>
2025-01-26 03:22:46 +0500

    change register api

--- Controllers/AuthApis/registerUser.js
+++ Controllers/AuthApis/registerUser.js
@@ -4,26 +4,26 @@ const UserModel = require("../../Models/UserModel");
 const registerUser = async (req, res) => {
   const { email, cnic, purpose } = req.body;
 
-  if (!email || !cnic || !purpose) {
-    return res.status(400).json({ message: "Missing required fields", success: false });
-  }
-
   try {
+    if (!body || Object.keys(body).length === 0) {
+      return res.status(400).json({ message: "Body is required", success: false });
+    }
     const userToken = generateOtp();
 
     let user = await UserModel.findOne({ email });
-    const userExists = user || new UserModel({ email, cnic, purpose });
-
-    if (!user) await userExists.save();
-
-    const tokenDoc = new tokenModel({ token: userToken, userRef: userExists._id });
-    await tokenDoc.save();
-
-    return res.status(200).json({ message: "Token Sent Successfully", success: true, token: userToken });
+    if (user) {
+      const tokens = new tokenModel({ token: userToken, userRef: user._id })
+      await tokens.save()
+      return res.status(400).json({ message: "Token Send Successfully", success: true, token: userToken })
+    }
+    const newUser = new UserModel({ email, cnic, purpose });
+    const tokens = new tokenModel({ token: userToken, userRef: newUser._id })
+    await newUser.save()
+    await tokens.save()
+    return res.status(400).json({ message: "Token Send Successfully", success: true, token: userToken })
 
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Something went wrong!", success: false });
+    res.status(500).json({ message: "Something went wrong!", success: false });
   }
 };
 
